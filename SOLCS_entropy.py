ec245b1df29c3cead4b693f0026b9e4797e8f4e2
Nakata Koya <n1310165@edu.cc.uec.ac.jp>
2019-12-29 13:38:30 +0900

    entropy()　スライス指定対応完了　hack:実装がよろしくないかも

--- SOLCS_entropy.py
+++ SOLCS_entropy.py
@@ -14,18 +14,18 @@ from tqdm import tqdm
 
 ADBIT_IDX = {"BLACK":[0,1,2], "RED":[0,1,3], "GREEN":[0,1,4], "BLUE":[0,1,5]}
 
-def fraction(nodes_rounded, adbits, actions):
+def fraction(nodes_rounded, adbits, adbit_idx, actions):
     ret_dic = {}
     #dic["key"] = "value"    
     
     for adbit in adbits:
         #print(adbit)
-        nodes_adbit = _extractWithAdbit(adbit, nodes_rounded)
+        nodes_extracted = _extractWithAdbit(adbit, adbit_idx, nodes_rounded)
         for act in actions:
             #print([act])
-            nodes_act = _extractWithAct(act, nodes_adbit)
+            nodes_act = _extractWithAct(act, nodes_extracted)
             
-            count_adbit = len(nodes_adbit)
+            count_adbit = len(nodes_extracted)
             count_act = len(nodes_act)
             
             frac = count_act / count_adbit
@@ -34,11 +34,11 @@ def fraction(nodes_rounded, adbits, actions):
 
     return ret_dic
 
-def entropy(nodes_rounded, adbits, actions):
+def entropy(nodes_rounded, adbits, adbit_idx, actions):
     ret_dic = {}
     
     for adbit in adbits:
-        nodes_adbit = _extractWithAdbit(adbit, nodes_rounded)
+        nodes_adbit = _extractWithAdbit(adbit, adbit_idx, nodes_rounded)
         entropy = 0
         for act in actions:
             nodes_act = _extractWithAct(act, nodes_adbit)
@@ -58,14 +58,14 @@ def entropy(nodes_rounded, adbits, actions):
         
     return ret_dic    
 
-def _extractWithAdbit(adbit, nodes_rounded): #adbitが一致するノードの抽出
+def _extractWithAdbit(adbit, adbit_idx, nodes_rounded): #adbitが一致するノードの抽出
     cluster = []
     for cl in nodes_rounded:
         if (adbit == cl[adbit_idx]).all():
             cluster.append(cl)
     return np.array(cluster)
 
-def _extractWithAct(act, nodes_rounded): #adbitが一致するノードの抽出
+def _extractWithAct(act, nodes_rounded): #actが一致するノードの抽出
     cluster = []
     for cl in nodes_rounded:
         if act == cl[-1]:
@@ -74,25 +74,32 @@ def _extractWithAct(act, nodes_rounded): #adbitが一致するノードの抽出
 
 if __name__ == "__main__":
     adbits_vals = [[0,0,0], [0,0,1], [0,1,0], [0,1,1], [1,0,0], [1,0,1], [1,1,0], [1,1,1]]
+    adbit_idx = ADBIT_IDX["BLACK"]
     actions = [0,1]
     
     resultDirStr = input("input dir path:")
     if resultDirStr == "":
-        resultDirStr = "exp_data\\debug_and_error_correct_with_updating_map\\teacher1_train10_error_corrected"
+        resultDirStr = "C:/Users/Nakata Koya/python/XCSandSOM_git/exp_data/teacher1_train10[0, 1, 2]_20191226111725/nodes.bin"
         
     print(resultDirStr)
     with open(resultDirStr, "rb") as nodes:
         nodes = pickle.load(nodes)
+        
+    print("choose color by input color key below:")
+    key = input('{"BLACK":[0,1,2], "RED":[0,1,3], "GREEN":[0,1,4], "BLUE":[0,1,5]}:')
+    
+    if key != "":
+        adbit_idx = ADBIT_IDX[key]
         
     #print(nodes.shape)
     
     nodes = np.round(nodes)
     #print(nodes)
     
-    #nodes_extracted_with_adbit = _extractWithAdbit([0,0,0], nodes)
-    #print(adbit00)
+    #nodes_extracted_with_adbit = _extractWithAdbit([0,0,0], adbit_idx, nodes)
+    #print(nodes_extracted_with_adbit)
     
-    fractions = fraction(nodes, adbits_vals, actions)
-    entropy_ = entropy(nodes, adbits_vals, actions)
+    fractions = fraction(nodes, adbits_vals, adbit_idx, actions)
+    entropy_ = entropy(nodes, adbits_vals, adbit_idx, actions)
     pprint.pprint(fractions)
     pprint.pprint(entropy_)
